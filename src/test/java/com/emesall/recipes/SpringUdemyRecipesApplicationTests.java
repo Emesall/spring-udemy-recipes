45d59d9390ec5f2b5e60fa2de7ea1ac20f871bb0
Emesall <mati1230@gmail.com>
2022-02-01 13:34:32 +0100

    Initial commit

---
+++ src/test/java/com/emesall/recipes/SpringUdemyRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emesall.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemyRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
