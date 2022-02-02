93f66cfaa176f4ab891fe50ecdf64e6489148547
Emesall <mati1230@gmail.com>
2022-02-02 15:45:58 +0100

    Added recipe and notes

---
+++ src/main/java/com/emesall/recipes/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.emesall.recipes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"","/","/index"})
+	public String getIndexPage() {
+		
+		return "index";
+	}
+}
---
+++ src/main/java/com/emesall/recipes/model/Notes.java
@@ -0,0 +1,31 @@
+package com.emesall.recipes.model;
+
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+public class Notes extends BaseEntity {
+
+	@OneToOne
+	private Recipe recipe;
+
+	@Lob
+	private String recipeNotes;
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+
+	public void setRecipeNotes(String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
+	
+	
+}
