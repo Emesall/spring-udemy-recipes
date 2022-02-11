c44fa055094c752d856acd5b683be49a9ee6301e
Emesall <mati1230@gmail.com>
2022-02-11 09:14:58 +0100

    Mock MVC test to recipesController added

--- src/test/java/com/emesall/recipes/controllers/RecipeControllerTest.java
+++ src/test/java/com/emesall/recipes/controllers/RecipeControllerTest.java
@@ -1,11 +1,13 @@
 package com.emesall.recipes.controllers;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.emesall.recipes.model.Recipe;
@@ -29,28 +33,33 @@ class RecipeControllerTest {
 	RecipeListService recipeListService;
 	@Mock
 	Model model;
-	
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
-		recipeController=new RecipeController(recipeListService);
-		
+		recipeController = new RecipeController(recipeListService);
+
+	}
+
+	@Test
+	void MockMvcTest() throws Exception {
+		MockMvc mockMvc=MockMvcBuilders.standaloneSetup(recipeController).build();
+		mockMvc.perform(get("/recipes")).andExpect(status().isOk()).andExpect(view().name("recipes/list"));
 	}
 
 	@Test
 	void testGetRecipeList() {
-		
-		Set<Recipe> recipes=new HashSet<Recipe>();
+
+		Set<Recipe> recipes = new HashSet<Recipe>();
 		recipes.add(new Recipe());
 		when(recipeListService.getRecipes()).thenReturn(recipes);
-		
-		ArgumentCaptor<Set<Recipe>> captor=ArgumentCaptor.forClass(Set.class);
-		
-		assertEquals("recipes/list",recipeController.getRecipeList(model) );
-		verify(recipeListService,times(1)).getRecipes();
-		verify(model,times(1)).addAttribute(eq("recipes"),captor.capture());
-		
-		Set<Recipe> actual=captor.getValue();
+
+		ArgumentCaptor<Set<Recipe>> captor = ArgumentCaptor.forClass(Set.class);
+
+		assertEquals("recipes/list", recipeController.getRecipeList(model));
+		verify(recipeListService, times(1)).getRecipes();
+		verify(model, times(1)).addAttribute(eq("recipes"), captor.capture());
+
+		Set<Recipe> actual = captor.getValue();
 		assertEquals(recipes.size(), actual.size());
 	}
 
