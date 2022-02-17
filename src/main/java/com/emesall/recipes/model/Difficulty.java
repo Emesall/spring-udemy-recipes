14b7f5937f7fd75deca030b77ec4a162576c1aa8
Emesall <mati1230@gmail.com>
2022-02-17 20:55:24 +0100

    difficulty drop down added

--- src/main/java/com/emesall/recipes/model/Difficulty.java
+++ src/main/java/com/emesall/recipes/model/Difficulty.java
@@ -2,5 +2,17 @@ package com.emesall.recipes.model;
 
 
 public enum Difficulty {
-	EASY, MEDIUM, HARD
+	EASY("Easy"), 
+	MEDIUM ("Medium"),
+	HARD ("Hard");
+	
+	private final String displayName;
+	
+	private Difficulty(String name) {
+		this.displayName=name;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
 }
