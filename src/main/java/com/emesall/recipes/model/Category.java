d87d11851d181fa051a4b8c434f37cc457ff4ab1
Emesall <mati1230@gmail.com>
2022-03-03 13:30:35 +0100

    Test

--- src/main/java/com/emesall/recipes/model/Category.java
+++ src/main/java/com/emesall/recipes/model/Category.java
@@ -3,12 +3,6 @@ package com.emesall.recipes.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -16,15 +10,12 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode(exclude = "recipes")
 @ToString(exclude = "recipes")
-@Entity
+
 public class Category {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long Id;
+	
+	private String Id;
 	private String name;
-
-	@ManyToMany(mappedBy = "categories")
 	private Set<Recipe> recipes = new HashSet<Recipe>();
 
 }
